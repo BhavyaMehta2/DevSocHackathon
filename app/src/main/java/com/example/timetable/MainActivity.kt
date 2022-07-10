f6c2b16150b8ac26b8203f256efafacd769224e9
Avinash <f20212933@goa.bits-pilani.ac.in>
2022-07-10 22:55:18 +0530

    Added Navigation Bar and its functionality and also added 4 buttons

---
+++ app/src/main/java/com/example/timetable/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.timetable
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val TimeTable=timetable()
+        val Announcements=announcements()
+        val Remainders=remainders()
+        val Contacts = contacts()
+
+        setCurrentFragment(TimeTable)
+        var BottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        BottomNavigationView.setOnItemSelectedListener{
+            when(it.itemId){
+                R.id.timetable->setCurrentFragment(TimeTable)
+                R.id.announcements->setCurrentFragment(Announcements)
+                R.id.remainders->setCurrentFragment(Remainders)
+                R.id.contacts->setCurrentFragment(Contacts)
+            }
+            true
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment,fragment)
+            commit()
+        }
+}
+
